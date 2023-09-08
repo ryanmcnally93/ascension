bd26be3d3dc0828a3479c3e1675cef4b415a1dfe
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-09-08 10:49:03 +0000

    Nav-links respond to hover

---
+++ static/js/base.js
@@ -0,0 +1,15 @@
+function mouseOver() {
+    document.getElementById('my-account').style.borderBottomColor = 'white';
+}
+  
+function mouseOut() {
+    document.getElementById('my-account').style.borderBottomColor = '#343a40';
+}
+
+function mouseOverCart() {
+    document.getElementById('cart').style.borderBottomColor = 'white';
+}
+  
+function mouseOutCart() {
+    document.getElementById('cart').style.borderBottomColor = '#343a40';
+}
