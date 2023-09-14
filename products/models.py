0c94613b16ff6a4ce5103a99551470d22b63f253
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-09-14 12:12:56 +0000

    Create Category and Product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=250)
+    friendly_name = models.CharField(max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    amount_sold = models.IntegerField(null=True, blank=True)
+    amount_of_reviews = models.IntegerField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    image_2 = models.ImageField(null=True, blank=True)
+    image_3 = models.ImageField(null=True, blank=True)
+    is_hire_room = models.BooleanField(default=False, null=True, blank=True)
+    is_offers_item = models.BooleanField(default=False, null=True, blank=True)
+    striked_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
