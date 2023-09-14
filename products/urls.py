2a9909ba0a87970876b7cdd52091e53c9ae66108
ryanmcnally93 <>
2023-09-14 17:39:57 +0000

    Product Information page css

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.products, name='products'),
+    path('<product_id>', views.product_information, name='product_information'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -8,3 +8,11 @@ def products(request):
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_information(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_information.html', context)
