de420e7f3ffef08c6bec3fa0acbf6583a347c635
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-09-23 10:14:21 +0000

    Add sort and direction filters

--- products/views.py
+++ products/views.py
@@ -5,11 +5,21 @@ from .models import Product
 def products(request):
     products = Product.objects.all()
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
+
+        if 'sort' in request.GET:
+            sort = request.GET['sort']
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sort = f'-{sort}'
+            products = products.order_by(sort)
 
         if 'shop' in request.GET:
             products = products.filter(is_hire_room=False)
@@ -17,8 +27,11 @@ def products(request):
         if 'sale' in request.GET:
             products = products.filter(is_offers_item=True)
 
+    current_sort = f'{sort}_{direction}'
+
     context = {
         'products': products,
+        'current_sort': current_sort,
     }
     return render(request, 'products/products.html', context)
 
