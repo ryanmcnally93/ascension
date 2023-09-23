6c98564702aeb2e8a1f78eed66b2f833a8e61a50
ryanmcnally93 <>
2023-09-23 08:44:44 +0000

    Add mainnav urls

--- record_studio/urls.py
+++ record_studio/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.record_studio_home, name='record_studio_home'),
+    path('meet_the_enginners/', views.meet_the_engineers, name='meet_the_engineers'),
 ]
--- record_studio/views.py
+++ record_studio/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def record_studio_home(request):
     return render(request, 'record_studio/home.html')
+
+
+def meet_the_engineers(request):
+    return render(request, 'record_studio/meet_the_engineers.html')
