be89aaa596c4a9fe5dd46d7c6fb6d0328640d35c
ryanmcnally93 <>
2023-09-28 18:55:54 +0000

    Can change between add button and quantity box

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def cart_contents(request):
@@ -5,6 +7,17 @@ def cart_contents(request):
     cart_products = []
     product_count = 0
     total = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+            product = get_object_or_404(Product, pk=item_id)
+            total += quantity * product.price
+            product_count += quantity
+            cart_products.append({
+                'item_id': item_id,
+                'quantity': quantity,
+                'product': product,
+            })
 
     context = {
         'cart_products': cart_products,
