0348f83eb092ac460ed5f8ad9606d3f133fdd3b9
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-10-05 09:23:24 +0000

    Remove all item functionality add

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 
 
@@ -6,8 +6,8 @@ def view_cart(request):
     return render(request, 'cart/cart.html')
 
 
-def add_to_cart(request, product_id):
-    redirect_url = request.POST.get('redirect_url')
+def alter_cart(request, product_id):
+    # redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
     quantity = int(request.POST.get('quantity'))
     
@@ -35,6 +35,25 @@ def add_to_cart(request, product_id):
             cart[product_id] += quantity
         else:
             cart[product_id] = quantity
+
+    if 'remove_all_of_item_product_info' in request.POST:
+        if product_id in cart:
+            cart.pop(product_id)
+            print('I AM SUCCESSFULLY RUNNING CORRECT CODE!')
+            request.session['cart'] = cart
+            return redirect('product_information', product_id=product_id)
+        else: 
+            # This is an issue! Reload issue causing this.
+            print('This item isnt in the cart!')
+
+    if 'remove_all_of_item_cart' in request.POST:
+        if product_id in cart:
+            cart.pop(product_id)
+            request.session['cart'] = cart
+            return redirect('view_cart')
+        else: 
+            # This is an issue! Reload issue causing this.
+            print('This item isnt in the cart!')
 
     request.session['cart'] = cart
     print(request.session['cart'])
