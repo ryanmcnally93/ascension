93a03fa8fa977861653e9872f9cdea541f9241de
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-10-05 15:06:42 +0000

    Start toast success style

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
-from django.http import HttpResponse
 from products.models import Product
 
 
@@ -14,7 +13,7 @@ def alter_cart(request, product_id):
     quantity = int(request.POST.get('quantity'))  
     date = ""
 
-    product = Product.objects.get(pk=product_id)
+    product = get_object_or_404(Product, pk=product_id)
 
     if 'date' in request.POST:
         date = request.POST.get('date')
@@ -26,7 +25,7 @@ def alter_cart(request, product_id):
 
     if 'increase-quantity' in request.POST:
         cart[product_id] += 1
-        messages.success(request, f'X {cart[product_id]}')
+        messages.success(request, f'Updated {product.name} quantity to {cart[product_id]}')
 
     if 'decrease-quantity' in request.POST:
         if product_id in cart:
@@ -35,35 +34,43 @@ def alter_cart(request, product_id):
                 messages.success(request, 'Successfully removed item from bag')
             else:
                 cart[product_id] -= 1
-                messages.success(request, f'X {cart[product_id]}')
+                messages.success(request, f'Updated {product.name} quantity to {cart[product_id]}')
         else:
             messages.error(request, 'This item isnt in the cart!')
 
     if 'add-quantity' in request.POST:
         if product_id in list(cart.keys()):
             cart[product_id] += quantity
-            messages.success(request, f'X {cart[product_id]}')
+            messages.success(request, f'Updated {product.name} quantity to {cart[product_id]}')
         else:
             cart[product_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
 
     if 'remove_all_of_item_product_info' in request.POST:
-        if product_id in cart:
-            cart.pop(product_id)
-            request.session['cart'] = cart
-            messages.success(request, 'Successfully removed item from bag')
-            return redirect('product_information', product_id=product_id)
-        else: 
-            messages.error(request, 'This item isnt in the cart!')
+        try:
+            if product_id in cart:
+                cart.pop(product_id)
+                request.session['cart'] = cart
+                messages.success(request, 'Successfully removed item from bag')
+                return redirect('product_information', product_id=product_id)
+            else: 
+                messages.error(request, 'This item isnt in the cart!')
+        except Exception as e:
+            messages.error(request, f'Error removing item {e}')
+            return HttpResponse(status=500)
 
     if 'remove_all_of_item_cart' in request.POST:
-        if product_id in cart:
-            cart.pop(product_id)
-            request.session['cart'] = cart
-            messages.success(request, 'Successfully removed item from bag')
-            return redirect('view_cart')
-        else:
-            messages.error(request, 'This item isnt in the cart!')
+        try:
+            if product_id in cart:
+                cart.pop(product_id)
+                request.session['cart'] = cart
+                messages.success(request, 'Successfully removed item from bag')
+                return redirect('view_cart')
+            else:
+                messages.error(request, 'This item isnt in the cart!')
+        except Exception as e:
+            messages.error(request, f'Error removing item {e}')
+            return HttpResponse(status=500)
 
     request.session['cart'] = cart
     print(request.session['cart'])
