0ac05764dda9a2bf1c0a7c520f59ec99a9a0e4d4
ryanmcnally93 <>
2023-10-06 19:18:56 +0000

    Start implementing Stripe

--- cart/static/cart/cart.js
+++
@@ -1,5 +0,0 @@
-let prices = document.getElementByClassNames('prices');
-let quantities = document.getElementByClassName('quantity-middle');
-let total_prices = document.getElementByClassName('total-price');
-
-$('total-price').each.value = $('prices').each.value * $('quantity-middle').each.value
