53d041099c687f1e7558255e50bec7f134df2421
ryanmcnally93 <>
2023-10-29 18:31:50 +0000

    Fix issue of items losing position when adding to cart

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('alter/<product_id>/', views.alter_cart, name='alter_cart'),
+    path('empty/', views.empty_cart, name='empty_cart'),
 ]
