e755fe7833151649c529c8b53db5b9c91be7c1d5
ryanmcnally93 <>
2023-11-02 14:27:23 +0000

    Complete Responsive Design

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,20 +1,70 @@
+if (window.innerWidth < 767) {
+    style = {
+        base: {
+            color: '#000',
+            fontSize: '0.9em',
+            '::placeholder': {
+                color: '#aab7c4'
+            }
+        },
+        invalid: {
+            color: '#dc3545',
+            iconColor: '#dc3545'
+        }
+    };
+} else {
+    style = {
+        base: {
+            color: '#000',
+            fontSize: '16px',
+            '::placeholder': {
+                color: '#aab7c4'
+            }
+        },
+        invalid: {
+            color: '#dc3545',
+            iconColor: '#dc3545'
+        }
+    };
+};
+
+window.addEventListener('resize', function(event) {
+    if (window.innerWidth < 767) {
+        card.style = {
+            base: {
+                color: '#000',
+                fontSize: '0.9em',
+                '::placeholder': {
+                    color: '#aab7c4'
+                }
+            },
+            invalid: {
+                color: '#dc3545',
+                iconColor: '#dc3545'
+            }
+        };
+    } else {
+        card.style = {
+            base: {
+                color: '#000',
+                fontSize: '16px !important',
+                '::placeholder': {
+                    color: '#aab7c4'
+                }
+            },
+            invalid: {
+                color: '#dc3545',
+                iconColor: '#dc3545'
+            }
+        };
+    };
+});
+
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
-var style = {
-    base: {
-        color: '#000',
-        fontSize: '16px',
-        '::placeholder': {
-            color: '#aab7c4'
-        }
-    },
-    invalid: {
-        color: '#dc3545',
-        iconColor: '#dc3545'
-    }
-};
+
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
