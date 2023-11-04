1bf848401469fc4c199a00f8495f649f5010145a
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-11-04 16:18:09 +0000

    Can add sessions to cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -9,16 +9,31 @@ def cart_contents(request):
     total = 0
     cart = request.session.get('cart', {})
 
-    for item_id, quantity in cart.items():
+    for item_id, item_data in cart.items():
+        if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            total += quantity * product.price
-            product_count += quantity
+            total += item_data * product.price
+            product_count += item_data
             cart_products.append({
                 'item_id': item_id,
-                'quantity': quantity,
+                'quantity': item_data,
                 'product': product,
-                'item_subtotal': quantity * product.price,
+                'item_subtotal': item_data * product.price,
             })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for date, time in item_data['session_datetime'].items():
+                quantity = len(time)
+                total += quantity * product.price
+                product_count += quantity
+                cart_products.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'date': date,
+                    'time': time,
+                    'item_subtotal': quantity * product.price,
+                })
 
     context = {
         'cart_products': cart_products,
--- cart/views.py
+++ cart/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from products.models import Product
 
 from checkout.models import Order, OrderLineItem
+from datetime import datetime
 
 
 def view_cart(request):
@@ -14,6 +15,8 @@ def alter_cart(request, product_id):
     cart = request.session.get('cart', {})
     quantity = int(request.POST.get('quantity'))  
     product = get_object_or_404(Product, pk=product_id)
+    date = None
+    time = None
 
     if 'increase-quantity' in request.POST:
         cart[product_id] += 1
@@ -25,8 +28,21 @@ def alter_cart(request, product_id):
                 cart.pop(product_id)
                 messages.success(request, 'Successfully removed item from cart')
             else:
-                cart[product_id] -= 1
-                messages.success(request, f'Updated {product.name} quantity to {cart[product_id]}')
+                if cart[product_id]['session_datetime']:
+                    date = list(cart[product_id]['session_datetime'].keys())[0]
+                    if len(cart[product_id]['session_datetime'][date]) == 1:
+                        cart.pop(product_id)
+                        request.session["cart"] = cart
+                        request.session.modified = True
+                        messages.success(request, 'Successfully removed item from cart')
+                    else:
+                        cart[product_id]['session_datetime'][date].popitem()
+                        request.session["cart"] = cart
+                        request.session.modified = True
+                        messages.success(request, f'Updated {product.name} quantity to {cart[product_id]}')
+                else:
+                    cart[product_id] -= 1
+                    messages.success(request, f'Updated {product.name} quantity to {cart[product_id]}')
         else:
             messages.error(request, 'This item isnt in the cart!')
 
@@ -38,20 +54,38 @@ def alter_cart(request, product_id):
             cart[product_id] = quantity
             messages.success(request, f'Added {product.name} to your cart')
 
-    # THIS IS THE VIEW FOR HIRE ROOM PURCHASE
+    # This code only run's if the product is a hire room
     if 'add-session' in request.POST:
-        if product_id in list(cart.keys()):
-            cart[product_id] += quantity
-            messages.success(request, f'Updated {product.name} quantity to {cart[product_id]}')
-            return redirect('view_cart')
-        else:
-            cart[product_id] = quantity
+        if request.POST.get('chosen-time') != 'none':
+            date_as_date = datetime.strptime(request.POST.get('date'), '%Y-%m-%d').date()
+            date = date_as_date.strftime('%d-%m-%Y')
+            time = request.POST.get('chosen-time')
+            # This is checking that the product is already in the bag
+            if product_id in list(cart.keys()):
+                # This is checking to see if there is an instance
+                # of this product with the same date in the bag
+                if date in cart[product_id]['session_datetime'].keys():
+                    # This is to ensure that a date and time cannot match
+                    if time in cart[product_id]['session_datetime'][date].keys():
+                        messages.error(request, 'Unfortunately, this session has already been booked.')
+                        return redirect('product_information', product_id=product_id)
+                    else:
+                        print('This product and date has another product in the bag')
+                        cart[product_id]['session_datetime'][date][time] = quantity
+                else:
+                    print('This product is in bag but not date')
+                    cart[product_id]['session_datetime'][date][time] = quantity
+            else:
+                print('This is the first of this product and date')
+                cart[product_id] = {'session_datetime': {date: {time: 1}}}
+
+            request.session["cart"] = cart
+            request.session.modified = True
             messages.success(request, f'Added {product.name} session to your cart')
             return redirect('view_cart')
-
-    if 'book' in request.POST:
-        # Handle Hire Room Bookings
-        return redirect('product_information', product_id=product_id)
+        else:
+            messages.error(request, 'You must select a date and time for this session!')
+            return redirect('product_information', product_id=product_id)
 
     if 'remove_all_of_item_product_info' in request.POST:
         try:
@@ -78,6 +112,10 @@ def alter_cart(request, product_id):
         except Exception as e:
             messages.error(request, f'Error removing item {e}')
             return HttpResponse(status=500)
+
+    if 'book' in request.POST:
+        # Handle Hire Room Bookings
+        return redirect('product_information', product_id=product_id)
 
     request.session['cart'] = cart
     print(request.session['cart'])
