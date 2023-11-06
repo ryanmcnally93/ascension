b595d2a7e56e0e5a3745df4ed0cb884e6b1e7489
ryanmcnally93 <>
2023-11-06 19:30:56 +0000

    Email error fix

--- ascension/settings.py
+++ ascension/settings.py
@@ -242,7 +242,7 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'admin@ascension.co.uk'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.setup.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
