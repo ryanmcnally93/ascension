599d0365eeccf5b459f110a8ff8470667d5b93b1
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-11-06 15:54:15 +0000

    Attempt deployment 1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ascension.wsgi:application
--- ascension/settings.py
+++ ascension/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 if os.path.exists("env.py"):
   import env 
@@ -28,7 +29,7 @@ SECRET_KEY = 'django-insecure-#7!vk5-i8t5@uae@ei=4npe^2o8uqw@%c(joq41+w0n8*t(k+0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ryanmcnally93-project-fo-jzagfoqlhn.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['ascension-music.herokuapp.com', 'localhost']
 
 # Application definition
 
@@ -123,12 +124,24 @@ WSGI_APPLICATION = 'ascension.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+ }
 
 
 # Password validation
@@ -206,3 +219,5 @@ SOCIALACCOUNT_PROVIDERS = {
         'GRAPH_API_URL': 'https://graph.facebook.com/v13.0',
     }
 }
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
