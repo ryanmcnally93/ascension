14c119f304a977c9ed74d2a164da21f7fbe996b7
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-11-06 16:04:48 +0000

    settings changes

--- ascension/settings.py
+++ ascension/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#7!vk5-i8t5@uae@ei=4npe^2o8uqw@%c(joq41+w0n8*t(k+0'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ascension-music-e0c178d908f4.herokuapp.com', 'localhost']
 
