e73fb079d44338a1ee43bfcd625769ff7c76b9f4
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-11-06 17:15:27 +0000

    Deployment attempt 4

--- ascension/settings.py
+++ ascension/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ascension-music-e0c178d908f4.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://ascension-music-e0c178d908f4.herokuapp.com/', 'localhost']
 
 # Application definition
 
