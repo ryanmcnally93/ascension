27dfeb80ae4354d00ab1f0e1be5f18fe92c63667
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-11-13 20:23:33 +0000

    Fix length of time in models

--- checkout/models.py
+++ checkout/models.py
@@ -46,7 +46,7 @@ class OrderLineItem(models.Model):
     quantity = models.IntegerField(null=False, blank=False, default=0)
     line_item_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
     date = models.CharField(max_length=15, blank=True, null=True)
-    time = models.CharField(max_length=5, blank=True, null=True)
+    time = models.CharField(max_length=254, blank=True, null=True)
 
     def save(self, *args, **kwargs):
         self.line_item_total = self.product.price * self.quantity
