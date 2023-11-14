c55c3698a3565188d7fe6a8dac76194cec5e3522
ryanmcnally93 <>
2023-11-14 15:21:29 +0000

    Fix default phone field

--- profiles/models.py
+++ profiles/models.py
@@ -7,7 +7,7 @@ from django.dispatch import receiver
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=50, null=True, blank=True)
-    default_phone_number = models.CharField(max_length=11, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
