ee06d2ca6695828a47af6bc78b98181b12885d09
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-11-16 11:55:02 +0000

    Start model testing

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Category, Product
 
-# Create your tests here.
+
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        Category.objects.create(name="electric_guitars", friendly_name="Electric Guitars")
+        Category.objects.create(name="drum_kits", friendly_name="Drum Kits")
+
+    def test_category_friendly_name(self):
+        electric_guitars = Category.objects.get(name="electric_guitars")
+        drum_kits = Category.objects.get(name="drum_kits")
+        self.assertEqual(electric_guitars.friendly_name, 'Electric Guitars')
+        self.assertEqual(drum_kits.friendly_name, 'Drum Kits')
